382605e5bd38153da874055824037441a370344e
Prashanth <yvprashanth@gmail.com>
2017-02-11 13:03:32 -0800

    changed the node back end server to return data to front end

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ var express    = require('express');
 var bodyParser = require('body-parser');
 var app        = express();
 var morgan     = require('morgan');
+var noteModel = require('./note');
 
 // configure app
 app.use(morgan('dev')); // log requests to the console
@@ -14,7 +15,14 @@ app.use(morgan('dev')); // log requests to the console
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port     = process.env.PORT || 8080; // set our port
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+  	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  	res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+    next();
+});
+
+var port     = process.env.PORT || 4500; // set our port
 
 var mongoose   = require('mongoose');
 mongoose.connect('mongodb://localhost/emberMongo'); // connect to our database
@@ -33,45 +41,51 @@ router.use(function(req, res, next) {
 	next();
 });
 
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+// test route to make sure everything is working (accessed at GET http://localhost:4500/api)
 router.get('/', function(req, res) {
 	res.json({ message: 'hooray! welcome to our api!' });	
 });
 
-// on routes that end in /bears
+// on routes that end in /goals
 // ----------------------------------------------------
 router.route('/goals')
-
-	// create a Goal (accessed at POST http://localhost:8080/bears)
+	// create a Goal (accessed at POST http://localhost:4500/goals)
 	.post(function(req, res) {
-		
 		var goal = new Goal();		// create a new instance of the goal model
-		goal.name = req.body.title;  // set the bears name (comes from the request)
+		goal.name = req.body.title;  // set the goals name (comes from the request)
 
 		goal.save(function(err) {
 			if (err)
 				res.send(err);
-
 			res.json({ message: 'Goal created!' });
 		});
-
-		
 	})
-
-	// get all the bears (accessed at GET http://localhost:8080/api/bears)
+	// get all the goals (accessed at GET http://localhost:4500/api/goals)
 	.get(function(req, res) {
-		Goal.find(function(err, bears) {
+		Goal.find(function(err, goals) {
 			if (err)
 				res.send(err);
-
-			res.json(bears);
+			res.json(goals);
 		});
 	});
 
-// on routes that end in /bears/:bear_id
+router.route('/notes')
+	// create a Goal (accessed at POST http://localhost:4500/goals)
+	// get all the goals (accessed at GET http://localhost:4500/api/goals)
+	.get(function(req, res) {
+		noteModel.find({},function(err,docs) {
+			if(err) {
+				res.send({error:err});
+			}
+			else {
+				res.send({note:docs});
+			}
+		});
+	});
+
+// on routes that end in /goals/:bear_id
 // ----------------------------------------------------
 router.route('/goals/:goal_id')
-
 	// get the goal with that id
 	.get(function(req, res) {
 		Goal.findById(req.params.bear_id, function(err, goal) {
@@ -84,18 +98,14 @@ router.route('/goals/:goal_id')
 	// update the goal with this id
 	.put(function(req, res) {
 		Goal.findById(req.params.bear_id, function(err, goal) {
-
 			if (err)
 				res.send(err);
-
 			goal.name = req.body.name;
 			goal.save(function(err) {
 				if (err)
 					res.send(err);
-
 				res.json({ message: 'goal updated!' });
 			});
-
 		});
 	})
 
@@ -106,7 +116,6 @@ router.route('/goals/:goal_id')
 		}, function(err, goal) {
 			if (err)
 				res.send(err);
-
 			res.json({ message: 'Successfully deleted' });
 		});
 	});
